d64dd1eac57f5b04a50b8ddd8c7c1b39dc924db5
matinslcs <ccplucasmartins@gmail.com>
2025-01-08 22:32:55 -0300

    Data

---
+++ SistemaControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+using SistemaControleDeContatos.Models;
+
+namespace SistemaControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<ContatoModel> Contato { get; set; }
+    }
+}
---
+++ SistemaControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SistemaControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public string Celular { get; set; }
+    }
+}
